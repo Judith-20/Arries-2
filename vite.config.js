6bda31096fd06836c39d758c2be6d28bd21e7c37
Judith-20 <ochubajudith@gmail.com>
2024-07-08 13:07:48 +0100

    fixed vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Arries-2/', // replace 'your-repo-name' with your actual repository name
 });
